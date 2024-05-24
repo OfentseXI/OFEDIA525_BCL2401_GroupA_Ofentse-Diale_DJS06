ce1cca1441ca4a5e516beab1e18287a9a759e7ee
OfentseXI <dofentse69@gmail.com>
2024-05-24 13:53:15 +0200

    log products

--- scripts.js
+++ scripts.js
@@ -4,15 +4,6 @@ const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', '
 // A list of names:
 const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
 
-// A list of products with prices:
-const products = [
-  { product: 'banana', price: "2" },
-  { product: 'mango', price: 6 },
-  { product: 'potato', price: ' ' },
-  { product: 'avocado', price: "8" },
-  { product: 'coffee', price: 10 },
-  { product: 'tea', price: '' },
-]
 
 //1. forEach Basics:
 // log each name to the console 
@@ -59,3 +50,21 @@ const namesToProvinces = names.reduce((acc, name, index) => {
   return acc;
 }, {});
 console.log(namesToProvinces);
+
+/*       Advanced Exercises       */
+
+// A list of products with prices:
+const products = [
+  { product: 'banana', price: "2" },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: "8" },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+
+const productsData = {
+//1. Log Products
+productName: products.map((name) => name.product).join(", ")
+
+}
